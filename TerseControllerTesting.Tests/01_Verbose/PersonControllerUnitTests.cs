76f148a4c72c5983999e0cdd2f1f7b2732a93623
Robert Moore <me@robdmoore.id.au>
2011-03-13 23:12:48 +0800

    Added testing project with first test method

---
+++ TerseControllerTesting.Tests/01_Verbose/PersonControllerUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+using TerseControllerTesting.Controllers;
+using TerseControllerTesting.Data;
+
+namespace TerseControllerTesting.Tests._01_Verbose
+{
+    [TestFixture]
+    class PersonControllerUnitTests
+    {
+        private IPersonRepository _repository;
+        private PersonController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repository = Substitute.For<IPersonRepository>();
+            _controller = new PersonController(_repository);
+        }
+
+        [Test]
+        public void Should_render_index_view_with_get_to_index()
+        {
+            // Arrange
+
+            // Act
+            var viewResult = _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult.ViewName, Is.EqualTo(""));
+        }
+    }
+}
