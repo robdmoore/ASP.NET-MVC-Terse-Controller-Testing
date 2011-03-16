c24c03f78e419e05e03be7c9b8264ebe55f73836
Robert Moore <me@robdmoore.id.au>
2011-03-16 23:52:41 +0800

    Added custom syntax to make tests terser

---
+++ TerseControllerTesting.Tests/04_CustomSyntax/MvcControllerTestingExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Web.Mvc;
+using MvcContrib.TestHelper;
+
+namespace TerseControllerTesting.Tests._04_CustomSyntax
+{
+    public static class MvcControllerTestingExtensions
+    {
+        public static ActionResult AssertHttpStatusReturned(this ActionResult actionResult)
+        {
+            var castedActionResult = actionResult as HttpStatusCodeResult;
+
+            if (castedActionResult == null)
+                throw new ActionResultAssertionException(string.Format("Expected action result to be a HttpStatusCodeResult, but instead received a {0}.", actionResult.GetType().Name));
+
+            return actionResult;
+        }
+
+        public static ActionResult AssertHttpStatusReturned(this ActionResult actionResult, int status)
+        {
+            actionResult.AssertHttpStatusReturned();
+
+            var castedActionResult = actionResult as HttpStatusCodeResult;
+
+            if (castedActionResult.StatusCode != status)
+                throw new ActionResultAssertionException(string.Format("Expected HTTP status code to be {0}, but instead received a {1}.", status, castedActionResult.StatusCode));
+
+            return actionResult;
+        }
+
+        public static ViewResult WithModel<T>(this ViewResult viewResult) where T : class
+        {
+            var castedModel = viewResult.Model as T;
+            if (castedModel == null)
+                throw new ViewResultModelAssertionException(string.Format("Expected view model to be of type {0}. It is actually of type {1}.", typeof(T).Name, viewResult.Model.GetType().Name));
+
+            return viewResult;
+        }
+
+        public static ViewResult WithModel<T>(this ViewResult viewResult, T expectedModel = default(T)) where T : class
+        {
+            viewResult.WithModel<T>();
+
+            var model = viewResult.Model as T;
+            if (model != expectedModel)
+                throw new ViewResultModelAssertionException("Expected view model to be passed in model, but in fact it was a different model.");
+
+            return viewResult;
+        }
+
+        public static Controller HasModelError(this Controller controller, string memberName, string equals = null, string contains = null)
+        {
+            if (!controller.ModelState.ContainsKey(memberName))
+                throw new ViewResultModelAssertionException(string.Format("Expected controller {0} to have a model error for member {1}, but none found.", controller, memberName));
+
+            if (!string.IsNullOrEmpty(equals) && controller.ModelState[memberName].Errors[0].ErrorMessage != equals)
+                throw new ViewResultModelAssertionException(string.Format("Expected controller {0} to have a model error for member {1} that equals '{2}', but instead found: '{3}'.", controller, memberName, equals, controller.ModelState[memberName].Errors[0].ErrorMessage));
+
+            if (!string.IsNullOrEmpty(contains) && controller.ModelState[memberName].Errors[0].ErrorMessage.Contains(contains))
+                throw new ViewResultModelAssertionException(string.Format("Expected controller {0} to have a model error for member {1} that contains '{2}', but instead found: '{3}'.", controller, memberName, contains, controller.ModelState[memberName].Errors[0].ErrorMessage));
+
+            return controller;
+        }
+
+        public static Controller AndModelError(this Controller controller, string memberName, string equals = null, string contains = null)
+        {
+            return controller.HasModelError(memberName, equals, contains);
+        }
+
+        public static T WithModelErrors<T>(this T controller) where T : Controller
+        {
+            controller.ModelState.AddModelError("key", "value");
+            return controller;
+        }
+    }
+
+    public class ViewResultModelAssertionException : Exception
+    {
+        public ViewResultModelAssertionException(string message) : base(message) {}
+    }
+}
---
+++ TerseControllerTesting.Tests/04_CustomSyntax/PersonControllerUnitTests.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using AutofacContrib.NSubstitute;
+using NSubstitute;
+using NUnit.Framework;
+using TerseControllerTesting.Controllers;
+using TerseControllerTesting.Data;
+using TerseControllerTesting.Models;
+using MvcContrib.TestHelper;
+
+namespace TerseControllerTesting.Tests._04_CustomSyntax
+{
+    [TestFixture]
+    class PersonControllerUnitTests
+    {
+
+        #region Setup
+
+        private AutoMock _autoMock;
+        private PersonController _controller;
+        private readonly Person _person = new Person {FirstName = "FirstName", LastName = "LastName", EmailAddress = "email@email.com"};
+        private const int PersonId = 1;
+
+        [SetUp]
+        public void Setup()
+        {
+            _autoMock = new AutoMock();
+            _controller = _autoMock.Resolve<PersonController>();
+            _person.Id = 0;
+        }
+
+        #endregion
+
+        #region Index
+
+        [Test]
+        public void Should_render_index_view_with_list_of_people_after_get_to_index()
+        {
+            _controller.Index().AssertViewRendered().ForView("").WithModel<IEnumerable<Person>>();
+        }
+
+        #endregion
+
+        #region Create
+
+        [Test]
+        public void Should_render_edit_view_after_get_to_create()
+        {
+            _controller.Create().AssertViewRendered().ForView("Edit");
+        }
+
+        [Test]
+        public void Should_return_edit_view_with_person_model_after_post_to_create_with_invalid_model()
+        {
+             _controller.WithModelErrors().Create(_person).AssertViewRendered().ForView("Edit").WithModel(_person);
+        }
+
+        [Test]
+        public void Should_return_edit_view_with_person_model_and_error_after_post_to_create_with_duplicate_email()
+        {
+            _autoMock.Resolve<IPersonRepository>().EmailBelongsToSomeoneElse(_person.EmailAddress).Returns(true);
+            _controller.Create(_person).AssertViewRendered().ForView("Edit")
+                .WithModel(_person);
+            _controller.HasModelError("EmailAddress", "The Email address must be unique; that email address already exists in the system.");
+        }
+
+        [Test]
+        public void Should_save_and_redirect_to_index_after_post_to_create_with_valid_person()
+        {
+            _autoMock.Resolve<IPersonRepository>().EmailBelongsToSomeoneElse(Arg.Any<string>()).Returns(false);
+            _controller.Create(_person).AssertActionRedirect().ToAction("Index");
+            _autoMock.Resolve<IPersonRepository>().Received().Save(_person);
+        }
+
+        #endregion
+
+        #region Edit
+
+        [Test]
+        public void Should_return_404_after_get_to_edit_with_invalid_id()
+        {
+            _controller.Edit(0).AssertHttpStatusReturned(404);
+        }
+
+        [Test]
+        public void Should_return_edit_view_with_person_model_after_get_to_edit_with_valid_id()
+        {
+            _autoMock.Resolve<IPersonRepository>().GetById(PersonId).Returns(_person);
+            _controller.Edit(PersonId).AssertViewRendered().ForView("").WithModel(_person);
+        }
+
+        [Test]
+        public void Should_return_edit_view_with_person_model_after_post_to_edit_with_invalid_model()
+        {
+            _controller.WithModelErrors().Edit(PersonId, _person).AssertViewRendered().ForView("").WithModel(_person);
+        }
+
+        [Test]
+        public void Should_return_edit_view_with_person_model_and_error_after_post_to_edit_with_duplicate_email()
+        {
+            _autoMock.Resolve<IPersonRepository>().EmailBelongsToSomeoneElse(_person.EmailAddress, PersonId).Returns(true);
+            _controller.Edit(PersonId, _person).AssertViewRendered().ForView("").WithModel(_person);
+            _controller.HasModelError("EmailAddress", "The Email address must be unique; that email address already exists in the system.");
+        }
+
+        [Test]
+        public void Should_save_and_redirect_to_index_after_post_to_edit_with_valid_person()
+        {
+            _autoMock.Resolve<IPersonRepository>().EmailBelongsToSomeoneElse(Arg.Any<string>(), Arg.Any<int>()).Returns(false);
+            _controller.Edit(PersonId, _person).AssertActionRedirect().ToAction("Index");
+            _autoMock.Resolve<IPersonRepository>().Received().Save(_person);
+            Assert.That(_person.Id, Is.EqualTo(PersonId));
+        }
+
+        #endregion
+    }
+}
