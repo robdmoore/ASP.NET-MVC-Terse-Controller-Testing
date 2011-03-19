62485719be1769ad7f9099235b8a6d1795fbf803
Robert Moore <me@robdmoore.id.au>
2011-03-19 20:27:06 +0800

    Adding test for one of the more complicated methods added to the new custom syntax

---
+++ TerseControllerTesting.Tests/05_CustomSyntax2/SomeOtherTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TerseControllerTesting.Controllers;
+
+namespace TerseControllerTesting.Tests._05_CustomSyntax2
+{
+    [TestFixture]
+    public class SomeOtherTests
+    {
+        [Test]
+        public void Test_redirect_to_other_controller()
+        {
+            Ensure.That(new TestController())
+                .Calling(c => c.Index())
+                .RedirectsTo<PersonController>(c => c.Index);
+        }
+    }
+}
---
+++ TerseControllerTesting.Tests/05_CustomSyntax2/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TerseControllerTesting.Tests._05_CustomSyntax2
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return new RedirectToRouteResult(new RouteValueDictionary(new { Controller = "Person", Action = "Index" }));
+        }
+
+    }
+}
